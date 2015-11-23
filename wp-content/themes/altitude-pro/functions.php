6cc32223ab898dd6dd1fcdf5b76c5a5e45ff6022
Jack Carrick Xiang Cheng <chengxianga2008@yahoo.com>
2015-11-24 02:01:36 +1100

    Jack update

--- wp-content/themes/altitude-pro/functions.php
+++ wp-content/themes/altitude-pro/functions.php
@@ -101,7 +101,16 @@ function romantic_do_nav(){?>
         <li><a class="romantic-float-shadow" href="<?php echo get_home_url(null, "romantic-experiences");?>">Experiences</a></li>
         <li><a class="romantic-float-shadow" href="<?php echo get_home_url(null, "blog");?>">Romantic Ideas</a></li>
         <li><a class="romantic-float-shadow" href="<?php echo get_home_url(null, "gift-vouchers");?>">Gift Vouchers</a></li>
-         </ul>
+        <li><a  href="<?php echo get_home_url(null, "shopping");?>"><i class="fa fa-2x fa-flip-horizontal fa-shopping-cart shopping-cart-icon" ></i></a></li>
+      	<li class="dropdown account-dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-lg fa-user user-icon"></i> <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Sign Up</a></li>
+            <li><a href="#">Log In</a></li>
+          </ul>
+        </li>
+          	
+      </ul>
     </div>
       
   </div>
